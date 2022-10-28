353cd1891fa507bce5c485973afec8948493d452
yoyoyoyo <112377134+yoyoyoyoyo6@users.noreply.github.com>
2022-10-28 23:52:46 +0800

    Create 排序法.c

---
+++ 数组/268.丢失的数字/排序法.c
@@ -0,0 +1,13 @@
+int cmp(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
+}
+
+int missingNumber(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(nums[0]), cmp);
+    for(int i =0; i<numsSize; i++){
+        if(nums[i] != i){
+            return i;
+        }
+    }
+    return numsSize;
+}
