a7f5ec6e0d6332460cabf0c9f1232e77000aa40f
yoyoyoyo <112377134+yoyoyoyoyo6@users.noreply.github.com>
2022-10-31 15:18:34 +0800

    Create 暴力法.cpp

---
+++ 数组/33.搜索旋转排序数组/暴力法.cpp
@@ -0,0 +1,9 @@
+int search(int* nums, int numsSize, int target){
+    int i;
+    for(i = 0; i<numsSize; i++){
+        if(nums[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
