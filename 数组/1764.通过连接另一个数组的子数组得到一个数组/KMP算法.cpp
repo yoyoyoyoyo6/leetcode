7a64769663097a77f472a1b573842acdb2c6b991
yoyoyoyo <112377134+yoyoyoyoyo6@users.noreply.github.com>
2022-12-18 23:45:19 +0800

    Create KMP算法.cpp

---
+++ 数组/1764.通过连接另一个数组的子数组得到一个数组/KMP算法.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int find(vector<int> &nums, int k, vector<int> &g) {
+        int m = g.size(), n = nums.size();
+        if (k + g.size() > nums.size()) {
+            return -1;
+        }
+        vector<int> pi(m);
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && g[i] != g[j]) {
+                j = pi[j - 1];
+            }
+            if (g[i] == g[j]) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = k, j = 0; i < n; i++) {
+            while (j > 0 && nums[i] != g[j]) {
+                j = pi[j - 1];
+            }
+            if (nums[i] == g[j]) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+    bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < groups.size(); i++) {
+            k = find(nums, k, groups[i]);
+            if (k == -1) {
+                return false;
+            }
+            k += groups[i].size();
+        }
+        return true;
+    }
+};
