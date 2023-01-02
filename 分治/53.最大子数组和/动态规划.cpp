70dfd95a8a720b57863a0f86c22ae3fdbd106550
yoyoyoyo <112377134+yoyoyoyoyo6@users.noreply.github.com>
2023-01-02 17:34:43 +0800

    Update 动态规划.cpp

--- 分治/53.最大子数组和/动态规划.cpp
+++ 分治/53.最大子数组和/动态规划.cpp
@@ -3,6 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int pre = 0, maxAns = nums[0];
         for (const auto &x: nums) {
+            // 状态转移方程
             pre = max(pre + x, x);
             maxAns = max(maxAns, pre);
         }
