b7f21b7de841e578dcb66394b5bad2fac5e4deab
yoyoyoyo <112377134+yoyoyoyoyo6@users.noreply.github.com>
2023-01-30 23:08:02 +0800

    Create 模拟.cpp

---
+++ 链表/1669.合并两个链表/模拟.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* preA = list1;
+        for(int i=0; i<a-1; ++i) {
+            preA = preA->next;
+        }
+        ListNode* preB = preA;
+        for(int i=0; i<b-a+2; ++i) {
+            preB = preB->next;
+        }
+        preA->next = list2;
+        while(list2->next != nullptr) {
+            list2 = list2->next;
+        }
+        list2->next = preB;
+        return list1;
+    }
+};
